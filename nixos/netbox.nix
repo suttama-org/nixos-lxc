903bbc06db747afe2e7eed8f66240f01d442687e
Frederick Morin <freddydumont@users.noreply.github.com>
2024-03-04 11:20:16 -0500

    Add nginx config to netbox

--- nixos/netbox.nix
+++ nixos/netbox.nix
@@ -1,7 +1,7 @@
 # https://nixos.wiki/wiki/NetBox
 # see the above link for a complete setup with reverse proxy
 
-{
+{ config, ... }: {
   networking.firewall = {
     enable = true;
     allowedTCPPorts = [ 80 443 8001 ];
@@ -10,6 +10,27 @@
   services.netbox = {
     enable = true;
     secretKeyFile = "/var/lib/netbox/secret-key-file";
-    listenAddress = "0.0.0.0";
+  };
+
+  services.nginx = {
+    enable = true;
+    user = "netbox";
+    recommendedTlsSettings = false;
+    clientMaxBodySize = "25m";
+    virtualHosts = {
+      "192.168.1.13" = {
+        listen = [
+          { addr = "192.168.1.13"; port = 8001; }
+        ];
+        locations = {
+          "/" = {
+            proxyPass = "http://localhost:8001";
+          };
+          "/static/" = {
+            root = "${config.services.netbox.dataDir}";
+          };
+        };
+      };
+    };
   };
 }
